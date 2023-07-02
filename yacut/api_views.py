c96a20c5c07ee89ec7dff7c4e16b8271716cc219
Sergey <san-8082@mail.ru>
2023-07-02 16:27:17 +0300

    rev

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -6,21 +6,28 @@ from flask import jsonify, request
 from . import app, db
 from .error_handlers import InvalidAPIUsage
 from .models import URLMap
-from .const import EMPTY_BODY_MASSEGE, URL_IS_NECESSARILY_MESSAGE, NOT_CORREKR_BODY_MESSAGE, PATTERN
+from .const import (
+    EMPTY_BODY_MASSEGE,
+    URL_IS_NECESSARILY_MESSAGE,
+    NOT_CORREKR_BODY_MESSAGE,
+    PATTERN,
+    NAME_TAKEN_MASSEGE_FIRST_PATH,
+    NAME_TAKEN_MASSEGE_SECOND_PATH
+)
 
 
 @app.route('/api/id/', methods=['POST'])
 def create_short_link():
     data = request.get_json()
     url = URLMap()
-    if not data:
+    if type(data) != dict:
         raise InvalidAPIUsage(EMPTY_BODY_MASSEGE)
     if 'url' not in data:
         raise InvalidAPIUsage(URL_IS_NECESSARILY_MESSAGE)
     if 'custom_id' in data:
         custom_id = data.get('custom_id')
         if not url.check_short_id_on_unic(custom_id):
-            raise InvalidAPIUsage(f'Имя "{custom_id}" уже занято.')
+            raise InvalidAPIUsage(f'{NAME_TAKEN_MASSEGE_FIRST_PATH}"{custom_id}"{NAME_TAKEN_MASSEGE_SECOND_PATH}')
         if custom_id == '' or custom_id is None:
             data['custom_id'] = url.get_unique_short_id()
         elif not re.match(PATTERN, custom_id):
@@ -29,8 +36,6 @@ def create_short_link():
         data['custom_id'] = url.get_unique_short_id()
     urlmap = URLMap.from_dict(data)
     db.session.add(urlmap)
-    # я не разобрался с save можете помочб разобраться?
-    # не понял что именно он должен делать, рпаспишите по подробнее
     db.session.commit()
     return jsonify(urlmap.to_dict()), HTTPStatus.CREATED
 
--- yacut/const.py
+++ yacut/const.py
@@ -4,6 +4,11 @@ from string import ascii_letters, digits
 EMPTY_BODY_MASSEGE = 'Отсутствует тело запроса'
 NOT_CORREKR_BODY_MESSAGE = 'Указано недопустимое имя для короткой ссылки'
 URL_IS_NECESSARILY_MESSAGE = '\"url\" является обязательным полем!'
+NAME_TAKEN_MASSEGE_FIRST_PATH = 'Имя '
+NAME_TAKEN_MASSEGE_SECOND_PATH = ' уже занято.'
+
+MAX_LEGHT = 16
+MIN_LEGHT = 1
 
 PATTERN = r'^[a-zA-Z\d]{1,16}$'
 PATTERN_FOR_GEN_URK = ascii_letters + digits
@@ -11,6 +16,3 @@ DICT_LABELS = {
     'original': 'url',
     'short': 'custom_id',
 }
-
-MAX_LEGHT = 16
-MIN_LEGHT = 1
